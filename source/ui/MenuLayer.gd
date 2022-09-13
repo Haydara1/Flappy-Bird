44b2a88d4dba972d9d488c2e9cafe6247eb3c18a
Haydara <haydaraal2005@gmail.com>
2022-09-12 20:05:27 -0700

    Added highscore label and menu button

--- source/ui/MenuLayer.gd
+++ source/ui/MenuLayer.gd
@@ -9,6 +9,7 @@ onready var high_score_label = $GameOverMenu/VBoxContainer/HighScoreLabel
 onready var game_over_menu = $GameOverMenu
 
 var game_started = false
+export var game = ""
 
 func _input(event):
 	if event.is_action_pressed("flap")  && !game_started :
@@ -18,9 +19,14 @@ func _input(event):
 		game_started = true
 		
 
-func init_game_over_menu(score):
+func init_game_over_menu(score, highscore):
 	score_label.text = "SCORE: " + str(score)
+	high_score_label.text = "HIGH: " + str(highscore)
 	game_over_menu.visible = true
 
 func _on_RestartButton_pressed():
+	get_tree().current_scene.Restart(game)
+
+
+func _on_MainMenu_pressed() -> void:
 	get_tree().reload_current_scene()
