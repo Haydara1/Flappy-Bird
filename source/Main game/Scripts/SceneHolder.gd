0736cc8afa18a1d51da643b8dba3f9d5e2166e7e
Haydara <haydaraal2005@gmail.com>
2022-09-12 18:56:09 -0700

    Implemented starting game functions

--- source/Main game/Scripts/SceneHolder.gd
+++ source/Main game/Scripts/SceneHolder.gd
@@ -1,7 +1,44 @@
 extends Node2D
 
+var classicGame = preload("res://Main game/Scenes/World.tscn")
+var colour = ""
+
+signal HalfAnimation
+
 func _ready():
 	play_scene_fade()
 
 func play_scene_fade():
 	$SceneFade/BlackRect/AnimationPlayer.play("SceneFade")
+
+
+func _on_Yellow_pressed() -> void:
+	colour = "y"
+	AddLevelChoosing()
+
+func _on_Green_pressed() -> void:
+	colour = "g"
+	AddLevelChoosing()
+
+func _on_Blue_pressed() -> void:
+	colour = "b"
+	AddLevelChoosing()
+
+func AddLevelChoosing():
+	$"Main menu".visible = false
+	play_scene_fade()
+	yield(self, "HalfAnimation")
+	$Choose_level.visible = true
+
+func ClassicGame():
+	play_scene_fade()
+	yield(self, "HalfAnimation")
+	$Choose_level.visible = false
+	
+	self.add_child(classicGame.instance())
+	get_child(get_child_count() - 1).colour = colour
+	get_child(get_child_count() - 1).setColour()
+
+
+func HalfAnimatinon():
+	emit_signal("HalfAnimation")
