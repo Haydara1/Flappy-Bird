115e692c2c8573ec1c1ea523e775bfc4bba59344
Haydara <haydaraal2005@gmail.com>
2022-09-14 19:49:29 -0700

    Added credits and red flappy sprite resource

--- source/Main game/Scripts/SceneHolder.gd
+++ source/Main game/Scripts/SceneHolder.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 var classicGame = preload("res://Main game/Scenes/World.tscn")
 var reverseGame = preload("res://Main game/Scenes/Reverse.tscn")
+var pathGame = preload("res://Main game/Scenes/Path.tscn")
+
+var credits = preload("res://SplashScreen.tscn")
+
 var colour = ""
 
 signal HalfAnimation
@@ -23,6 +27,14 @@ func _on_Green_pressed() -> void:
 
 func _on_Blue_pressed() -> void:
 	colour = "b"
+	AddLevelChoosing()
+
+func _on_Koopabtn_pressed() -> void:
+	colour = "k"
+	AddLevelChoosing()
+
+func _on_Red_pressed() -> void:
+	colour = "r"
 	AddLevelChoosing()
 
 func AddLevelChoosing():
@@ -51,12 +63,28 @@ func ReverseGame():
 	get_child(get_child_count() - 1).colour = colour
 	get_child(get_child_count() - 1).setColour()
 
+func PathGame():
+	Physics2DServer.area_set_param(get_viewport().find_world_2d().get_space(), Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2(0, 1))
+	play_scene_fade()
+	yield(self, "HalfAnimation")
+	$Choose_level.visible = false
+	
+	self.add_child(pathGame.instance())
+	get_child(get_child_count() - 1).colour = colour
+	get_child(get_child_count() - 1).setColour()
+
 func Restart(node):
 	remove_child(get_child(get_child_count() - 1))
 	if node == "Classic":
 		ClassicGame()
 	elif node == "Reverse":
 		ReverseGame()
+	elif node == "Path":
+		PathGame()
 
 func HalfAnimatinon():
 	emit_signal("HalfAnimation")
+
+
+func _on_Credits_pressed() -> void:
+	add_child(credits.instance())
