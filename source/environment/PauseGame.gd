2dd393d00177d4ea29cefc425a7782fbd5142f27
Haydara <haydaraal2005@gmail.com>
2022-09-16 20:32:43 -0700

    Plays pause sound

--- source/environment/PauseGame.gd
+++ source/environment/PauseGame.gd
@@ -2,14 +2,18 @@ extends Area2D
 
 var paused = false
 
+onready var pause_sound = $PauseSound
+
 func _input(event: InputEvent) -> void:
 	if(event.is_action_pressed("flap")):
 		if get_parent().get_parent().over:
 			return
 		if paused == true:
+			pause_sound.play()
 			get_tree().paused = !paused
 			paused = !paused
 			return
 		if get_global_mouse_position().y > 720:
+			pause_sound.play()
 			get_tree().paused = !paused
 			paused = !paused
