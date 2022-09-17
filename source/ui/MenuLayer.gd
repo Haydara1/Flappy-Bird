25938b6c78f21b8681b1eda7ced553f7e1842f2d
Haydara <haydaraal2005@gmail.com>
2022-09-16 20:37:02 -0700

    Implemented sound effects and bug fixes

--- source/ui/MenuLayer.gd
+++ source/ui/MenuLayer.gd
@@ -8,6 +8,7 @@ onready var score_label = $GameOverMenu/VBoxContainer/ScoreLabel
 onready var high_score_label = $GameOverMenu/VBoxContainer/HighScoreLabel
 onready var game_over_menu = $GameOverMenu
 onready var game_over_sprite = $GameOver
+onready var button_press = $ButtonPress
 
 signal over
 
@@ -29,19 +30,22 @@ func init_game_over_menu(score, highscore):
 	high_score_label.text = "HIGH: " + str(highscore)
 	game_over_sprite.visible = true
 	
-	tween.interpolate_property(game_over_sprite, "modulate:a", 0, 1, 1)
+	tween.interpolate_property(game_over_sprite, "modulate:a", 0, 1, 1.5)
 	tween.start()
 	yield(tween, "tween_completed")
 	
-	tween.interpolate_property(game_over_sprite, "modulate:a", 1, 0, 1)
+	tween.interpolate_property(game_over_sprite, "modulate:a", 1, 0, 1.5)
 	tween.start()
 	yield(tween, "tween_completed")
 	
 	game_over_menu.visible = true
 
 func _on_RestartButton_pressed():
+	button_press.play()
 	get_tree().current_scene.Restart(game)
 
 
 func _on_MainMenu_pressed() -> void:
+	button_press.play()
+	yield(button_press, "finished")
 	var _bin = get_tree().reload_current_scene()
