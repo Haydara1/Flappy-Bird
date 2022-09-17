922028b21a7402f98cf0596591baf8b616b0ebf9
Haydara <haydaraal2005@gmail.com>
2022-09-17 15:42:55 -0700

    Implemented level extreme

--- source/Main game/Scripts/SceneHolder.gd
+++ source/Main game/Scripts/SceneHolder.gd
@@ -3,6 +3,7 @@ extends Node2D
 var classicGame = preload("res://Main game/Scenes/World.tscn")
 var reverseGame = preload("res://Main game/Scenes/Reverse.tscn")
 var pathGame = preload("res://Main game/Scenes/Path.tscn")
+var bossGame = preload("res://Main game/Scenes/Boss.tscn")
 
 var nightBackground = preload("res://assets/textures/background-night.png")
 
@@ -90,6 +91,18 @@ func PathGame():
 	get_child(get_child_count() - 1).colour = colour
 	get_child(get_child_count() - 1).setColour()
 
+
+func BossGame() -> void:
+	buttonPress.play()
+	Physics2DServer.area_set_param(get_viewport().find_world_2d().get_space(), Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2(0, 1))
+	play_scene_fade()
+	yield(self, "HalfAnimation")
+	$Choose_level.visible = false
+	
+	self.add_child(bossGame.instance())
+	get_child(get_child_count() - 1).colour = colour
+	get_child(get_child_count() - 1).setColour()
+
 func Restart(node):
 	remove_child(get_child(get_child_count() - 1))
 	if node == "Classic":
@@ -98,6 +111,8 @@ func Restart(node):
 		ReverseGame()
 	elif node == "Path":
 		PathGame()
+	elif node == "Boss":
+		BossGame()
 
 func HalfAnimatinon():
 	emit_signal("HalfAnimation")
@@ -117,3 +132,5 @@ func _on_Exit_pressed() -> void:
 func _on_Settings_pressed() -> void:
 	buttonPress.play()
 	add_child(settings.instance())
+
+
